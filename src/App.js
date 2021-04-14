c4071efd2b37c793ce1239b8eb5c1ab20ddce78c
unknown <zim21den21@gmail.com>
2021-04-14 16:40:26 +0300

    btn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
 import Header from './component/Header'
+
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Header />
     </div>
   );
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,19 @@
-function Header() {
+import PropTypes from 'prop-types';
+import Button from './Button'
+
+function Header({ title }) {
     return (
-        <header>
-            <h1>To-do</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' />
         </header>
     )
+
+}
+Header.defaultProps = {
+    title: 'Tracker'
+}
+Header.propTypes = {
+    title: PropTypes.string.isRequired
 }
 export default Header
